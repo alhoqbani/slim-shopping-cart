9ea20c7d2970bed235ead25f37c2ed95a145e395
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-06-21 08:18:18 -0700

    Add Nameroute home

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Psr\Http\Message\ServerRequestInterface;
 use Slim\Views\Twig;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-$app->get('/home', HomeController::class . ':index');
+$app->get('/home', HomeController::class . ':index')->setName('home');
 
 $app->get('/', function (ServerRequestInterface $request, ResponseInterface $response, Twig $twig, Capsule $capsule) {
         \App\Models\User::all();
